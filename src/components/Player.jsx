aa51bcf02aa24c7cc80755d8a3903aea49351a7c
Stefanos-katsivelos <stef.katsivelos@gmail.com>
2024-08-08 01:30:26 +0300

    OnChange - event.target.value

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,28 +1,34 @@
 import { useState } from "react";
 
-export default function Player({name, symbol}) {
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
-    setIsEditing((editing) => !editing);  // => Always use a callback function to update a state.
-    
+    setIsEditing((editing) => !editing); // => Always use a callback function to update a state.
   }
 
-  let playerName = <span className="player-name">{name}</span>
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
   // let btnCaption = 'Edit';
 
   if (isEditing) {
-    playerName = <input type="text" required value={name} />
+    editablePlayerName = (
+      <input type="text" required Value={playerName} onChange={handleChange} />
+    );
     // btnCaption = 'Save';
   }
 
   return (
     <li>
-      <span className="player">{playerName}
+      <span className="player">
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
-    
-  )
+  );
 }
