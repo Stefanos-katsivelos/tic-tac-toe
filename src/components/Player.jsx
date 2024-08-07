240f54aa5efeddaab026bfff32badc02321d1d08
Stefanos-katsivelos <stef.katsivelos@gmail.com>
2024-08-07 06:02:23 +0300

    line6-Player correction

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,7 +3,7 @@ export default function Player({name, symbol}) {
     <li>
       <span className="player">
         <span className="player-name">{name}</span>
-        <span className="player-sumbol">{symbol}</span>
+        <span className="player-symbol">{symbol}</span>
       </span>
       <button>Edit</button>
     </li>
